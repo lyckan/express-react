6cd822040801f64a1edb8b7f5180d31fd8b29cc7
lyckan <lycksen@gmail.com>
2019-12-27 19:50:20 +0100

    setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,40 @@
 var gulp = require("gulp");
 var LiveServer = require("gulp-live-server");
 var browserSync = require("browser-sync");
+var broswerify = require("browserify");
+var reactify = require("reactify");
+var source = require("vinyl-source-stream");
 
-gulp.task("live-server", function() {
+/*gulp.task("live-server", function() {
   var server = new LiveServer("server/main.js");
   server.start();
 });
-
-gulp.task("serve", gulp.parallel("live-server"), function() {
+*/
+gulp.task("bundle", function() {
+  return broswerify({
+    entries: "app/main.jsx",
+    debug: true
+  })
+    .transform(reactify)
+    .bundle()
+    .pipe(source("app.js"))
+    .pipe(gulp.dest("./.temp"));
+});
+/*
+gulp.task("serve", gulp.series("live-server"), function() {
   browserSync.init(null, {
-    proxy: "http://localhost:7777",
+    proxy: "localhost:7777",
     port: 9001
   });
 });
+*/
+function serve() {
+  var server = new LiveServer("server/main.js");
+  server.start();
+  browserSync.init(null, {
+    proxy: "localhost:7777",
+    port: 9001
+  });
+}
+
+exports.serve = serve;
